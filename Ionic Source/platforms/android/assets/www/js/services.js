145aa2eedcf158b9613d84227fa598411419dc77
DusanNesic95 <dusannesic001@gmail.com>
2016-11-01 15:38:50 +0100

    Sredjena kompletna funkcionalnost

---
+++ Ionic Source/platforms/android/assets/www/js/services.js
@@ -0,0 +1,443 @@
+angular.module('starter.services', ['firebase'])
+
+.factory("Areacode", function($firebaseArray) {
+	var item = firebase.database().ref('areacode');
+	return $firebaseArray(item);
+})
+
+.factory("Test", function($firebaseArray) {
+	var item = firebase.database().ref('user');
+	return $firebaseArray(item);
+})
+
+.factory("Login", function($firebaseObject) {
+	return function(){
+		var item = firebase.database().ref('login');
+		return {
+			get: function(phone){
+				item = item.child(phone);
+				return $firebaseObject(item)
+			},
+			getId: function(phone){
+				item = item.child(phone+'/id');
+				return $firebaseObject(item);
+			},
+			getEmail: function(email){
+				item = item.orderByChild('email/value').equalTo(email);
+				return $firebaseObject(item);
+			},
+			set: function(phone){
+				item = item.child('maxID');
+				maxID = $firebaseObject(item);
+				maxID.$loaded(function(){
+					userID = maxID.$value +1;
+					var user = firebase.database().ref('login').child(phone);
+					user.child('id').set(userID);
+					user.child('role').set('normal');
+					item.set(userID);
+				});
+			},
+			changePass: function(phone,pass){
+				item.child(phone+'/password').set(pass);
+			},
+			changeRole: function(key, role){
+				item.child(key+'/role').set(role);
+			}
+		}
+	}
+})
+
+.factory("Users", function($firebaseArray) {
+	return function(){
+		var item = firebase.database().ref('user');
+		return {
+			get: function() {
+				return $firebaseArray(item);
+			},
+			changeRole: function(name, role) {
+				item.child(name+'/role').set(role);
+			}
+		}
+	}
+})
+
+.factory("User", function($firebaseObject, $firebaseArray) {
+	return function(id){
+		var item = firebase.database().ref('user/'+id);
+		return {
+			get: function(){ return $firebaseObject(item) },
+			set: function(data){ item.set(data); },
+			getAvatar: function(){
+				item = item.child('avatar');
+				return $firebaseObject(item)
+			},
+			getName: function(){
+				item = item.child('name');
+				return $firebaseObject(item)
+			},
+			getPhone: function(){
+				item = item.child('phone');
+				return $firebaseObject(item)
+			},
+			getLastSign: function(){
+				item = item.child('lastSign');
+				return $firebaseObject(item)
+			},
+			editAvatar: function(avatar){
+				item = item.child('avatar').set(avatar);
+			},
+			editName: function(name){
+				item.child('name').set(name);
+			},
+			editPhone: function(phone){
+				item.child('phone').set(phone);
+			},
+			editGender: function(gender){
+				item.child('gender').set(gender);
+			},
+			update: function(){
+				var now = new Date().getTime();
+				item.child('lastSign').set(now);
+			},
+			filter: function(gender){
+				var filter = firebase.database().ref('user');
+				if(gender != 'All') filter = filter.orderByChild('gender').equalTo(gender);
+				return $firebaseArray(filter);
+			},
+			changeRole: function(name, role) {
+				item.child(name+'/role').set(role);
+			}
+		}
+	}
+})
+
+.factory("Block", function($firebaseObject){
+	return function(id){
+		var item = firebase.database().ref('block/'+id);
+		return {
+			get: function(user){
+				item = item.child(user);
+				return $firebaseObject(item)
+			},
+			remove: function(user){
+				item.child(user).remove();
+				var friend = firebase.database().ref('block/'+user);
+				friend.child(id).remove();
+			},
+			block: function(user){
+				item.child(user).set(true);
+				var friend = firebase.database().ref('block/'+user);
+				friend.child(id).set(true);
+			}
+		}
+	}
+})
+
+.factory("Notification", function($firebaseObject) {
+	return function(id){
+		var item = firebase.database().ref('notification/'+id);
+		return {
+			get: function(){ return $firebaseObject(item) },
+			post: function(friend){
+				item = firebase.database().ref('notification/'+friend);
+				var data = $firebaseObject(item.child('messagesNew'));
+				data.$loaded(function(){
+					var newNotification = Number(data.$value) + 1;
+					item.child('messagesNew').set(newNotification);
+				});
+			},
+			update: function(readed){
+				var data = $firebaseObject(item.child('messagesNew'));
+				data.$loaded(function(){
+					var newNotification = Number(data.$value) - readed;
+					if(angular.isNumber(data.$value) && newNotification > 0) item.child('messagesNew').set(newNotification);
+					else item.child('messagesNew').remove();
+				});
+			}
+		}
+	}
+})
+
+.factory("Messages", function($firebaseArray, $firebaseObject) {
+	return function(id){
+		var item = firebase.database().ref('messages/'+id);
+		return {
+			get: function(){ return $firebaseArray(item) },
+			getUnread: function(friend){
+				item = item.child(friend);
+				return $firebaseObject(item.child('unread'));
+			},
+			post: function(friend,data){
+				item = item.child(friend);
+				item.set(data);
+				var messages = firebase.database().ref('messages/'+friend).child(id);
+				messages.child('content').set(data.content);
+				messages.child('time').set(data.time);
+				var unread = $firebaseObject(messages.child('unread'));
+				unread.$loaded(function(){
+					var newUnread = unread.$value + 1;
+					if(angular.isNumber(unread.$value)) messages.child('unread').set(newUnread);
+					else messages.child('unread').set(1);
+				});
+			},
+			delete: function(friend, unread){
+				var userUnread = firebase.database().ref('notification/'+id+'/messagesNew');
+				var nowUnread = $firebaseObject(userUnread);
+				nowUnread.$loaded(function(){
+					if(angular.isNumber(nowUnread.$value) && nowUnread.$value >0){
+						userUnread.set(nowUnread.$value -unread)
+					}
+					item.child(friend).remove();
+					item = firebase.database().ref('detailMessages/'+id).child(friend);
+					item.remove();
+				});
+			},
+			reset: function(friend){
+				item = item.child(friend);
+				item.child('unread').remove();
+			},
+			clear: function(friend){ item.child(friend).remove() }
+		}
+	}
+})
+
+.factory("DetailMessages", function($firebaseArray, $ionicScrollDelegate) {
+	return function(id){
+		var item = firebase.database().ref('detailMessages/'+id);
+		return {
+			get: function(friend){
+				item = item.child(friend);
+				return $firebaseArray(item)
+			},
+			post: function(friend,data){
+				item = item.child(friend);
+				var onComplete = function(){
+					$ionicScrollDelegate.$getByHandle('mainScroll').scrollBottom();
+				};
+				item.push().set(data, onComplete);
+				var friend = firebase.database().ref('detailMessages/'+friend).child(id);
+				data.from = 1;
+				friend.push().set(data);
+			},
+			delete: function(friend,id){
+				item.child(friend+'/'+id).remove();
+			},
+			clear: function(friend){ item.child(friend).remove() }
+		}
+	}
+})
+
+.factory("Contacts", function($firebaseArray, $firebaseObject) {
+	return function(id){
+		var item = firebase.database().ref('contacts/'+id);
+		return {
+			get: function(){ return $firebaseArray(item) },
+			getFriend: function(){ return $firebaseObject(item) },
+			post: function(friend){
+				item.child(friend).set(true);
+				var friend = firebase.database().ref('contacts/'+friend);
+				friend.child(id).set(true);
+			},
+			remove: function(friend){
+				item.child(friend).remove();
+				var friend = firebase.database().ref('contacts/'+friend);
+				friend.child(id).remove();
+			}
+		}
+	}
+})
+
+.factory("ContactsRecommended", function($firebaseArray) {
+	return function(id){
+		var item = firebase.database().ref('contactsRecommended/'+id);
+		return {
+			get: function(){ return $firebaseArray(item) },
+			post: function(friend){
+				item = firebase.database().ref('contactsRecommended/'+friend);
+				item.child(id).set(true);
+			},
+			remove: function(friend){
+				item.child(friend).remove();
+			}
+		}
+	}
+})
+
+.factory("Groups", function($firebaseArray, $firebaseObject) {
+	return function(id){
+		var item = firebase.database().ref('groups/'+id);
+		return {
+			getUser: function(number){
+				if(angular.isDefined(number) && angular.isNumber(number)){
+					item = item.child('user').limitToFirst(number);
+				} else item = item.child('user');
+				return $firebaseArray(item)
+			},
+			getName: function(){
+				var name = item.child('name');
+				return $firebaseObject(name);
+			},
+			getLast: function(){
+				var last = firebase.database().ref('groups').child('last');
+				return $firebaseObject(last);
+			},
+			getNumUser: function(){
+				item = item.child('countUser');
+				return $firebaseObject(item)
+			},
+			create: function(id,me,count,list,name){
+				var groups = firebase.database().ref('groups');
+				groups.child('last').set(id);
+				groups = groups.child(id);
+				groups.child('countUser').set(count+1);
+				groups.child('user/'+me).set(true);
+				if(name && angular.isString(name)) groups.child('name').set(name);
+				angular.forEach(list, function(value,key){
+					groups.child('user/'+key).set(true);
+				});
+			},
+			add: function(list,count){
+				var numUser = $firebaseObject(item.child('countUser'));
+				numUser.$loaded(function(){
+					item.child('countUser').set(numUser.$value +count);
+					angular.forEach(list, function(value,key){
+						item.child('user/'+key).set(true);
+					});
+				});
+			},
+			leave: function(userid){
+				item.child('user/'+userid).remove();
+				var numUser = $firebaseObject(item.child('countUser'));
+				numUser.$loaded(function(){
+					if(numUser.$value <= 3){
+						var user = $firebaseArray(item.child('user'));
+						var userGroups = firebase.database().ref('userGroups');
+						user.$loaded(function(){
+							angular.forEach(user, function(value){
+								userGroups.child(value.$id+'/'+id).remove();
+							});
+							item.remove();
+						});
+					}
+					item.child('countUser').set((numUser.$value)-1);
+				});
+			},
+			changeName: function(name){
+				item.child('name').set(name);
+			}
+		}
+	}
+})
+
+.factory("DetailGroups", function($firebaseArray, $ionicScrollDelegate) {
+	return function(id){
+		var item = firebase.database().ref('detailGroups/'+id);
+		return {
+			get: function(){ return $firebaseArray(item) },
+			post: function(data){
+				var onComplete = function(){
+					$ionicScrollDelegate.scrollBottom();
+				};
+				item.push().set(data, onComplete);
+			}
+		}
+	}
+})
+
+.factory("UserGroups", function($firebaseArray) {
+	return function(id){
+		var item = firebase.database().ref('userGroups/'+id);
+		return {
+			get: function(){ return $firebaseArray(item) },
+			post: function(me,list,id){
+				var groups = firebase.database().ref('userGroups');
+				groups.child(me+'/'+id).set(true);
+				angular.forEach(list, function(value,key){
+					groups.child(key+'/'+id).set(true);
+				});
+			},
+			add: function(id,list){
+				var groups = firebase.database().ref('userGroups');
+				angular.forEach(list, function(value,key){
+					groups.child(key+'/'+id).set(true);
+				});
+			},
+			leave: function(id){
+				item.child(id).remove();
+			}
+		}
+	}
+})
+
+.factory("Settings", function($firebaseObject) {
+	return function(id){
+		var item = firebase.database().ref('settings/'+id);
+		return {
+			get: function(child){
+				item = item.child(child);
+				return $firebaseObject(item)
+			},
+			change: function(child,data){
+				item.child(child).set(data);
+			}
+		}
+	}
+})
+
+.factory('Camera', function($q) {
+   return {
+      getPicture: function(options) {
+         var q = $q.defer();
+         navigator.camera.getPicture(function(result) {
+            q.resolve(result);
+         }, function(err) {
+            q.reject(err);
+         }, options);
+         return q.promise;
+      }
+   }
+})
+
+.factory('Location', function($firebaseObject, $ionicPopup){
+	return function(id){
+		var item = firebase.database().ref('location/'+id);
+		return {
+			get: function(){ return $firebaseObject(item) },
+			getNearby: function(location){
+				item = item.parent.orderByChild('nearby').equalTo(location);
+				return $firebaseObject(item);
+			},
+			update: function(){
+				function onSuccess(data){
+					var location = {};
+					location.lat = data.coords.latitude;
+					location.lng = data.coords.longitude;
+					location.nearby = data.coords.latitude.toFixed(2)+'_'+data.coords.longitude.toFixed(2);
+					item.set(location);
+				}
+				function onError(err){
+					var confirmPopup = $ionicPopup.confirm({
+					template: '<p><b class="dark">Error: The Geolocation service failed.</b></p>You should enable to share your position on your mobile',
+					cssClass: 'popup-confirm-delete',
+					buttons: [
+						{
+							text: 'Read More',
+							type: 'button-clear col-66 button-no-delete',
+							onTap: function(){
+								var ref = cordova.InAppBrowser.open('https://support.google.com/maps/answer/2839911?co=GENIE.Platform%3DAndroid&hl=en&oco=1', '_blank', 'location=yes');
+							}
+						},
+						{
+							text: 'OK',
+							type: 'button-clear col-33 button-no-delete'
+						}
+					]
+					});
+				}
+				navigator.geolocation.getCurrentPosition(onSuccess, onError);
+			}
+		}
+	}
+})
+
+;
---
+++ Ionic Source/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,416 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "id": "cordova-plugin-camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "id": "cordova-plugin-camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+        "id": "cordova-plugin-camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/contacts.js",
+        "id": "cordova-plugin-contacts.contacts",
+        "clobbers": [
+            "navigator.contacts"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/Contact.js",
+        "id": "cordova-plugin-contacts.Contact",
+        "clobbers": [
+            "Contact"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/convertUtils.js",
+        "id": "cordova-plugin-contacts.convertUtils"
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactAddress.js",
+        "id": "cordova-plugin-contacts.ContactAddress",
+        "clobbers": [
+            "ContactAddress"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactError.js",
+        "id": "cordova-plugin-contacts.ContactError",
+        "clobbers": [
+            "ContactError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactField.js",
+        "id": "cordova-plugin-contacts.ContactField",
+        "clobbers": [
+            "ContactField"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactFindOptions.js",
+        "id": "cordova-plugin-contacts.ContactFindOptions",
+        "clobbers": [
+            "ContactFindOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactName.js",
+        "id": "cordova-plugin-contacts.ContactName",
+        "clobbers": [
+            "ContactName"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactOrganization.js",
+        "id": "cordova-plugin-contacts.ContactOrganization",
+        "clobbers": [
+            "ContactOrganization"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-contacts/www/ContactFieldType.js",
+        "id": "cordova-plugin-contacts.ContactFieldType",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/DirectoryEntry.js",
+        "id": "cordova-plugin-file.DirectoryEntry",
+        "clobbers": [
+            "window.DirectoryEntry"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/DirectoryReader.js",
+        "id": "cordova-plugin-file.DirectoryReader",
+        "clobbers": [
+            "window.DirectoryReader"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/Entry.js",
+        "id": "cordova-plugin-file.Entry",
+        "clobbers": [
+            "window.Entry"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/File.js",
+        "id": "cordova-plugin-file.File",
+        "clobbers": [
+            "window.File"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/FileEntry.js",
+        "id": "cordova-plugin-file.FileEntry",
+        "clobbers": [
+            "window.FileEntry"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/FileError.js",
+        "id": "cordova-plugin-file.FileError",
+        "clobbers": [
+            "window.FileError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/FileReader.js",
+        "id": "cordova-plugin-file.FileReader",
+        "clobbers": [
+            "window.FileReader"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/FileSystem.js",
+        "id": "cordova-plugin-file.FileSystem",
+        "clobbers": [
+            "window.FileSystem"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/FileUploadOptions.js",
+        "id": "cordova-plugin-file.FileUploadOptions",
+        "clobbers": [
+            "window.FileUploadOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/FileUploadResult.js",
+        "id": "cordova-plugin-file.FileUploadResult",
+        "clobbers": [
+            "window.FileUploadResult"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/FileWriter.js",
+        "id": "cordova-plugin-file.FileWriter",
+        "clobbers": [
+            "window.FileWriter"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/Flags.js",
+        "id": "cordova-plugin-file.Flags",
+        "clobbers": [
+            "window.Flags"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/LocalFileSystem.js",
+        "id": "cordova-plugin-file.LocalFileSystem",
+        "clobbers": [
+            "window.LocalFileSystem"
+        ],
+        "merges": [
+            "window"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/Metadata.js",
+        "id": "cordova-plugin-file.Metadata",
+        "clobbers": [
+            "window.Metadata"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/ProgressEvent.js",
+        "id": "cordova-plugin-file.ProgressEvent",
+        "clobbers": [
+            "window.ProgressEvent"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/fileSystems.js",
+        "id": "cordova-plugin-file.fileSystems"
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/requestFileSystem.js",
+        "id": "cordova-plugin-file.requestFileSystem",
+        "clobbers": [
+            "window.requestFileSystem"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/resolveLocalFileSystemURI.js",
+        "id": "cordova-plugin-file.resolveLocalFileSystemURI",
+        "merges": [
+            "window"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/browser/isChrome.js",
+        "id": "cordova-plugin-file.isChrome",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/android/FileSystem.js",
+        "id": "cordova-plugin-file.androidFileSystem",
+        "merges": [
+            "FileSystem"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/fileSystems-roots.js",
+        "id": "cordova-plugin-file.fileSystems-roots",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-file/www/fileSystemPaths.js",
+        "id": "cordova-plugin-file.fileSystemPaths",
+        "merges": [
+            "cordova"
+        ],
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media-capture/www/CaptureAudioOptions.js",
+        "id": "cordova-plugin-media-capture.CaptureAudioOptions",
+        "clobbers": [
+            "CaptureAudioOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media-capture/www/CaptureImageOptions.js",
+        "id": "cordova-plugin-media-capture.CaptureImageOptions",
+        "clobbers": [
+            "CaptureImageOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media-capture/www/CaptureVideoOptions.js",
+        "id": "cordova-plugin-media-capture.CaptureVideoOptions",
+        "clobbers": [
+            "CaptureVideoOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media-capture/www/CaptureError.js",
+        "id": "cordova-plugin-media-capture.CaptureError",
+        "clobbers": [
+            "CaptureError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media-capture/www/MediaFileData.js",
+        "id": "cordova-plugin-media-capture.MediaFileData",
+        "clobbers": [
+            "MediaFileData"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media-capture/www/MediaFile.js",
+        "id": "cordova-plugin-media-capture.MediaFile",
+        "clobbers": [
+            "MediaFile"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media-capture/www/capture.js",
+        "id": "cordova-plugin-media-capture.capture",
+        "clobbers": [
+            "navigator.device.capture"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+        "id": "cordova-plugin-x-socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+        "clobbers": [
+            "cordova.plugins.notification.local",
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-core.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Core",
+        "clobbers": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-util.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Util",
+        "merges": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-app-event": "1.2.0",
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-camera": "2.2.0",
+    "cordova-plugin-console": "1.0.3",
+    "cordova-plugin-contacts": "2.1.0",
+    "cordova-plugin-device": "1.1.2",
+    "cordova-plugin-file": "4.2.0",
+    "cordova-plugin-geolocation": "2.2.0",
+    "cordova-plugin-inappbrowser": "1.4.0",
+    "cordova-plugin-media-capture": "1.3.0",
+    "cordova-plugin-network-information": "1.2.1",
+    "cordova-plugin-splashscreen": "3.2.2",
+    "cordova-plugin-statusbar": "2.1.3",
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-x-socialsharing": "5.1.3",
+    "de.appplant.cordova.plugin.local-notification": "0.8.4",
+    "ionic-plugin-keyboard": "2.2.1"
+};
+// BOTTOM OF METADATA
+});
